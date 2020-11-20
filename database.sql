ab94eba8710d9244e3a5edb403644d266a7699cc
madiian <yann.madelaine15@gmail.com>
2020-11-20 16:11:12 +0400

    modif
    ajout de fonction sur le create

---
+++ database.sql
@@ -0,0 +1,13 @@
+use random_recette
+
+create table recette(
+
+    id int(11) auto_increment primary key,
+    nom_recette varchar(50) not null,
+    description varchar(250) not null,
+    ingredients varchar(250) not null
+
+
+
+
+);
